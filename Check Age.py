12bbded6a62bcbd3a8631de6e7afc609b720d061
TQ110-2012 <tanizqasim@gmail.com>
2025-12-23 21:31:42 +0500

    Add files via upload

---
+++ Check Age.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:"))
+if age == 10 :
+    print("You are allowed to enrol in this class!")
+elif age <= 20:
+    print("You can enrol in this class.")
+else:
+    print("You can not enrol in this class")
